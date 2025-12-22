e0d86c69fc895c9cad4123a94c63e9bbd62def6d
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-12-22 11:59:22 +0100

    add helm charts and root endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express, {type Request, type Response} from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({});
+});
+
 app.get("/items", (req: Request, res: Response) => {
   res.send(["item1", "item2", "item3", "item4"]);
 });
