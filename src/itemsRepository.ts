cda833bd397e87e46832ee31d2c17733a6a81012
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-12-23 10:49:34 +0100

    use env variables

--- src/itemsRepository.ts
+++ src/itemsRepository.ts
@@ -5,11 +5,11 @@ export class ItemsRepository {
 
   constructor() {
     this.pool = new Pool({
-      user: "myuser",
-      host: "postgres-release-postgresql",
-      database: "mydatabase",
-      password: "cN7g6N1yhe%",
-      port: 5432,
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
     });
   }
 }
