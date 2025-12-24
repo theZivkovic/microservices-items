b078ddf8d97fab94bcbae0a35ed890a8a2b277d2
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-12-24 22:35:49 +0100

    remove logs

--- src/auditLogsClient.ts
+++ src/auditLogsClient.ts
@@ -4,7 +4,6 @@ class AuditLogsClient {
   constructor() {}
 
   addAuditLog(event_type: string, payload: any) {
-    console.log(process.env.AUDIT_LOGS_SERVICE_URL);
     return axios.post(process.env.AUDIT_LOGS_SERVICE_URL as string, {
       event_type,
       payload,
