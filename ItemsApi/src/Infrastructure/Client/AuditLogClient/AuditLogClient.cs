dcca1dbb45da128e077f45ea76f411ce159070b9
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-12-30 11:39:51 +0100

    add poly

--- ItemsApi/src/Infrastructure/Client/AuditLogClient/AuditLogClient.cs
+++ ItemsApi/src/Infrastructure/Client/AuditLogClient/AuditLogClient.cs
@@ -1,15 +1,35 @@
-namespace Infrastructure.Client;
 
-using System.Text.Json;
 using Domain.Enums;
 using Domain.Interfaces.Clients;
 using Domain.Models;
+
+using Polly;
+using Polly.Retry;
+using System.Net;
+
+namespace Infrastructure.Client;
 
 public class AuditLogClient(HttpClient httpClient) : IAuditLogClient
 {
     public async Task AddItemEvent(AuditLogEventType eventType, Item item)
     {
-        using var response = await httpClient.PostAsJsonAsync("/api/audit-logs", AuditLogRequest.Create(item, eventType).Value!);
-        response.EnsureSuccessStatusCode();
+        var pipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions
+            {
+                ShouldHandle = new PredicateBuilder()
+                    .Handle<HttpRequestException>(ex => ex.StatusCode is HttpStatusCode code && ((int)code == 429 || (int)code >= 500)),
+                MaxRetryAttempts = 4,
+                Delay = TimeSpan.FromSeconds(1),
+                BackoffType = DelayBackoffType.Exponential,
+                UseJitter = true,
+            })
+            .Build();
+
+        await pipeline.ExecuteAsync(async ct =>
+                {
+                    using var response = await httpClient.PostAsJsonAsync("/api/audit-logs", AuditLogRequest.Create(item, eventType).Value!, ct);
+                    response.EnsureSuccessStatusCode();
+                    return response;
+                });
     }
 }
